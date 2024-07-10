4b4993fdb17b56083f2b2e6bca845f0be537fbd5
Saikat5943 <159873989+Saikat5943@users.noreply.github.com>
2024-07-10 10:28:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let main=document.getElementById('main');
+console.log(main);
+let nav=document.getElementById('nav');
+console.log(nav);
+let containers=document.getElementsByClassName('container');
+console.log(containers);
+// let sel=document.quarySelector('#nav>li');
+// console.log("selector returns",sel);
+let sel=document.querySelectorAll('#nav>li');
+console.log("Selector returns ",sel);
